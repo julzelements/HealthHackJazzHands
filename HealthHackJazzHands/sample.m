ff2aafa5957d5a934fe960f7e74a1c0902335541
Julian Scharf <jscharf@thoughtworks.com>
2016-10-15 19:53:46 +1000

    Libraries OpenEars have been configured with ObjC header file

---
+++ HealthHackJazzHands/sample.m
@@ -0,0 +1,13 @@
+//
+//  sample.m
+//  HealthHackJazzHands
+//
+//  Created by Julian Scharf on 15/10/2016.
+//  Copyright © 2016 Julian Scharf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <OpenEars/OELanguageModelGenerator.h>
+#import <OpenEars/OEAcousticModel.h>
+#import <OpenEars/OEPocketsphinxController.h>
+#import <OpenEars/OEEventsObserver.h>
