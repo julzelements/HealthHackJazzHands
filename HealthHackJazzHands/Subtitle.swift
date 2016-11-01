a24dfb13f4a881b2fc6992829221acf0c78717e9
Julian Scharf <jscharf@thoughtworks.com>
2016-11-02 09:12:16 +1000

    deleted Star Wars sub comments

--- HealthHackJazzHands/Subtitle.swift
+++ HealthHackJazzHands/Subtitle.swift
@@ -11,38 +11,7 @@
 
 class Subtitle {
   var Stanzas = [
-    
-//    Stanza(index:  1, lines: [" Jyn, whatever I do," ],                                             on_at:   8.451 ,off_at:  10.092),
-//    Stanza(index:  2, lines: ["I do it to protect you."],                                           on_at:  12.470, off_at:  14.120),
-//    Stanza(index:  3, lines: ["You understand?"],                                                   on_at:  14.840, off_at:  16.091),
-//    Stanza(index:  4, lines: ["I understand."],                                                     on_at:  16.446, off_at:  17.474),
-//    Stanza(index:  5, lines: ["You want to get out of here?"],                                      on_at:  37.531, off_at:  38.921),
-//    Stanza(index:  6, lines: ["Our rebellion, is all that remains\n to push back the Empire."],     on_at:  39.954, off_at:  43.917),
-//    Stanza(index:  7, lines: ["I think you might be able\nto help us."],                            on_at:  45.412, off_at:  47.142),
-//    Stanza(index:  8, lines: ["When was the last time\nyou were in contact with your father?"],     on_at:  47.393, off_at:  49.701),
-//    Stanza(index:  9, lines: ["What is this?!"],                                                    on_at:  49.951, off_at:  51.426),
-//    Stanza(index: 10, lines: ["It appears he's critical\n to the development of a super weapon."],  on_at:  53.677, off_at:  57.265),
-//    Stanza(index: 11, lines: ["If my father built this thing,"],                                    on_at:  60.148, off_at:  61.560),
-//    Stanza(index: 12, lines: ["we need to find him."],                                              on_at:  62.206, off_at:  63.355),
-//    Stanza(index: 13, lines: ["All right, how many do I need?"],                                    on_at:  63.604, off_at:  65.522),
-//    Stanza(index: 14, lines: ["They are requesting a call sign."],                                  on_at:  68.150, off_at:  70.402),
-//    Stanza(index: 15, lines: ["Rogue, Rogue One!"],                                                 on_at:  71.584, off_at:  73.322),
-//    Stanza(index: 16, lines: ["The power"],                                                         on_at:  76.575, off_at:  77.576),
-//    Stanza(index: 17, lines: ["that we are dealing with here\n is immeasurable."],                  on_at:  77.826, off_at:  81.456),
-//    Stanza(index: 18, lines: ["If this Empire has this kind of power,\n what chance do we have?"],  on_at:  82.361, off_at:  85.959),
-//    Stanza(index: 19, lines: ["We have hope."],                                                     on_at:  91.466, off_at:  92.759),
-//    Stanza(index: 20, lines: ["Rebellions are built on hope."],                                     on_at:  93.703, off_at:  95.052),
-//    Stanza(index: 21, lines: ["They have no idea we're coming."],                                   on_at: 101.433, off_at: 103.102),
-//    Stanza(index: 22, lines: ["Take hold of this moment!\n  The Force is strong."],                 on_at: 105.605, off_at: 108.316),
-//    Stanza(index: 23, lines: ["Make ten men feel like a hundred."],                                 on_at: 109.108, off_at: 111.486),
-//    Stanza(index: 24, lines: ["We'll take the next chance,"],                                       on_at: 114.322, off_at: 115.865),
-//    Stanza(index: 25, lines: ["and the next."],                                                     on_at: 117.742, off_at: 118.951),
-//    Stanza(index: 26, lines: ["You're all rebels, aren't you?"],                                    on_at: 121.441, off_at: 122.831),
-//    Stanza(index: 27, lines: ["Save the rebellion!"],                                               on_at: 125.331, off_at: 126.500),
-//    Stanza(index: 28, lines: ["Save the dream!"],                                                   on_at: 127.626, off_at: 128.669),
-//    
-
-    
+        
     Stanza(index:  1, lines: [ " (CLICKING)" ],                                                                             on_at: 0 * 60.0 + 28.600, off_at: 0 * 60.0 + 30.352),
     Stanza(index:  2, lines: [ " I don't give shitty jobs." ],                                                              on_at: 0 * 60.0 + 31.320, off_at: 0 * 60.0 + 33.072),
     Stanza(index:  3, lines: [ " If a good man..." ],                                                                       on_at: 0 * 60.0 + 33.200, off_at: 0 * 60.0 + 34.599),
